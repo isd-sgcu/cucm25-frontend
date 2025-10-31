505964422cce9acbbb40f64d7c6b4aaf7d4e24fc
BadLuckZ <thanagorn8802@gmail.com>
2025-11-01 01:38:35 +0700

    feat: system closed popup

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,9 +1,16 @@
+import SystemClosedPopup from "@/components/popup/SystemClosedPopup";
+import { useSystemStatus } from "@/context/SystemStatus";
+
 function MainLayout({ children }: { children: React.ReactNode }) {
+  const { isClosed } = useSystemStatus();
+
   return (
     <div className="w-full bg-black h-screen flex justify-center">
       <div className="w-full sm:max-w-md min-h-screen overflow-auto bg-white flex flex-col gap-8">
         {children}
       </div>
+
+      {isClosed && <SystemClosedPopup />}
     </div>
   );
 }
