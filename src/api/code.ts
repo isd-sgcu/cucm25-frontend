1c0024927057d555101c06312b05be98e6279b3f
BadLuckZ <thanagorn8802@gmail.com>
2025-12-08 15:44:00 +0700

    fix: redeem inteface

--- src/api/code.ts
+++ src/api/code.ts
@@ -2,7 +2,7 @@ import Axios from '@/lib/axios'
 
 const BASE_URL = __API_ROOT__ + '/api/code'
 
-export interface RedeemResponse {
+export interface RedeemInterface {
   success: boolean
   message: string
   rewardCoin: number
@@ -10,12 +10,17 @@ export interface RedeemResponse {
   transactionId: number
 }
 
-export const redeem = async (codeString: string): Promise<RedeemResponse> => {
+export interface RedeemResponse {
+  success: boolean
+  data: RedeemInterface
+}
+
+export const redeem = async (codeString: string): Promise<RedeemInterface> => {
   try {
     const response = await Axios.post<RedeemResponse>(`${BASE_URL}/redeem`, {
       codeString,
     })
-    return response.data
+    return response.data.data
   } catch (error: any) {
     const status = error.response?.status
     if (status === 400) throw new Error('Missing required fields')
