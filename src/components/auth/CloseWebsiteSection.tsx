8a251f4c95c812fd098e26f1a521bfa7dd601a78
BadLuckZ <thanagorn8802@gmail.com>
2025-12-16 16:38:30 +0700

    fix: close system image lost

--- src/components/auth/CloseWebsiteSection.tsx
+++ src/components/auth/CloseWebsiteSection.tsx
@@ -9,7 +9,7 @@ function CloseWebsiteSection() {
   const navigate = useNavigate()
   return (
     <div className='flex flex-col items-center justify-center gap-4'>
-      <img src='/public/close-website.png' alt='Close Website' className='w-36' />
+      <img src='/close-website.png' alt='Close Website' className='w-36' />
       <div className='flex flex-col gap-2'>
         <h1 className='font-medium text-center display-small-emphasized'>ปิดระบบชั่วคราว</h1>
         <div className='flex flex-col gap-1 pb-6'>
